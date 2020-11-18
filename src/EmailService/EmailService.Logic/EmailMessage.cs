eee16d3a10d29789e771ce913550e7f74dde2616
Jerzy Wickowski <jurek@deployacademy.pl>
2020-11-18 14:42:29 +0100

    add simple EmailMessage class

---
+++ src/EmailService/EmailService.Logic/EmailMessage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmailService.Logic
+{
+    public class EmailMessage
+    {
+        public EmailMessage(string[] to, string from, string content)
+        {
+            _to = to.ToList();
+            From = from;
+            Content = content;
+        }
+
+        private List<string> _to;
+        public string[] To
+        {
+            get
+            {
+                return _to.ToArray();
+            }
+        }
+
+        public string From { get; private set; }
+        public string Content { get; private set; }
+        public int Id { get; private set; }
+    }
+}
