f6dab1ee066739ac5f814110bfa088561c29fd86
Jerzy Wickowski <jurek@deployacademy.pl>
2020-11-18 14:06:00 +0100

    add Logic.UnitTests project

---
+++ src/EmailService/EmailService.Logic.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace EmailService.Logic.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
