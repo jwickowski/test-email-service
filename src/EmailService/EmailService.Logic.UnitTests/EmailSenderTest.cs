9b3e33fa15db5976a5a4521fc6ce00d75e98f3d3
Jerzy Wickowski <jurek@deployacademy.pl>
2020-11-19 17:14:40 +0100

    add test when_email_is_send_then_it_should_be_passed_to_protocol_sender

--- src/EmailService/EmailService.Logic.UnitTests/EmailSenderTest.cs
+++ src/EmailService/EmailService.Logic.UnitTests/EmailSenderTest.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Transactions;
 using NSubstitute;
 using Xunit;
 
@@ -6,12 +10,54 @@ namespace EmailService.Logic.UnitTests
 {
     public class EmailSenderTest
     {
+        EmailMessage message = new EmailMessage(new[] { "to@wp.pl" }, "from@wp.pl", "Topic", "Hallo");
+
+
+        [Fact]
+        public void when_email_is_send_then_it_should_be_passed_to_protocol_sender()
+        {
+            var pendingEmailsGetter = Substitute.For<IPendingEmailsGetter>();
+            var protocolEmailSender = Substitute.For<IProtocolEmailSender>();
+            pendingEmailsGetter.GetPendingMails().Returns(new[] {new ConcreteEmailMessage{EmailId = Guid.NewGuid(), EmailMessage = message} });
+            var _emailSender = new EmailSender(pendingEmailsGetter, protocolEmailSender);
+            _emailSender.SendPendingEmails();
+
+            protocolEmailSender.Received().SendExternal(Arg.Any<MailMessage>());
+        }
         [Fact]
         public void when_email_is_sent_and_from_field_is_empty_then_use_default_from_config()
         {
-            //var pendingEmailsGetter = Substitute.For<IPendingEmailsGetter>();
-            //Guid emailId = _emailSender.SendPendingEmails();
+    
             Assert.True(false);
         }
+    }
+
+    public class EmailSender
+    {
+        public EmailSender(IPendingEmailsGetter pendingEmailsGetter, IProtocolEmailSender protocolEmailSender)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendPendingEmails()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IProtocolEmailSender
+    {
+        void SendExternal(MailMessage mail);
+    }
+
+    public interface IPendingEmailsGetter
+    {
+        public IEnumerable<ConcreteEmailMessage> GetPendingMails();
+    }
+
+    public class ConcreteEmailMessage
+    {
+        public Guid EmailId { get; set; }
+        public EmailMessage EmailMessage { get; set; }
     }
 }
