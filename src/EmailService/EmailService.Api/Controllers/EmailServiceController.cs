f7cbdc8f38a287be19c54b79ab53f40b75851a58
Jerzy Wickowski <jurek@deployacademy.pl>
2020-11-20 12:01:41 +0100

    Api project

---
+++ src/EmailService/EmailService.Api/Controllers/EmailServiceController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmailService.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmailServiceController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<EmailServiceController> _logger;
+
+        public EmailServiceController(ILogger<EmailServiceController> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
